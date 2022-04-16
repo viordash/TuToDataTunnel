94fec6ea51494f576d412476fe47a30a8c58da74
Vitaliy Orazov <viordash@mail.ru>
2022-04-16 12:27:55 +0300

    WIP. parsing command line args

---
+++ Projects/TutoProxy/TutoProxy.Server/CommandLine/PortsArgument.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine.Parsing;
+
+namespace TutoProxy.Server.CommandLine {
+    internal class PortsArgument {
+        public List<int> Ports { get; set; } = new List<int>();
+
+        public static ParseArgument<PortsArgument?> ToParseArgument() =>
+            result => Parse(result);
+
+        static PortsArgument? Parse(ArgumentResult result) {
+            if(result.Tokens.Count != 1) {
+                result.ErrorMessage = "Ports can only be parsed with single token";
+                return default;
+            }
+            var token = result.Tokens[0];
+            var postArgument = new PortsArgument();
+
+            return postArgument;
+        }
+    }
+}
--- Projects/TutoProxy/TutoProxy.Server/Program.cs
+++ Projects/TutoProxy/TutoProxy.Server/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Reflection;
+using TutoProxy.Server.CommandLine;
+
+class Program {
+    public static async Task<int> Main(string[] args) {
+        var hostArg = new Argument<string>("host", "Local host address");
+        var postArg = new Argument<PortsArgument?>(name: "ports", parse: PortsArgument.ToParseArgument(), description: "Listened port (80,81,443,700-900)");
+
+        var testArg = new Argument<string>("test");
+        var verboseOpt = new Option<bool>("--verbose", "Show the verbose logs");
+        var rootCommand = new RootCommand {
+            hostArg,
+            postArg,
+            testArg,
+            verboseOpt,
+        };
+
+        rootCommand.Description = "Прокси сервер TuTo";
+
+        rootCommand.SetHandler((string host, PortsArgument ports, string test, bool verbose) => {
+            Console.WriteLine($"The value for host is: {host}");
+            Console.WriteLine($"The value for ports is: {ports}");
+            Console.WriteLine($"The value for test is: {test}");
+            Console.WriteLine($"The value for verbose is: {verbose}");
+
+            StartServer(host, ports, verbose);
+        }, hostArg, postArg, testArg, verboseOpt);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void StartServer(string host, PortsArgument ports, bool verbose) {
+        Console.WriteLine("{0} {1}", Assembly.GetExecutingAssembly().GetName().Name, Assembly.GetExecutingAssembly().GetName().Version);
+        Console.WriteLine("Прокси сервер TuTo, хост {0}, портs {1}", host, ports);
+    }
+}
