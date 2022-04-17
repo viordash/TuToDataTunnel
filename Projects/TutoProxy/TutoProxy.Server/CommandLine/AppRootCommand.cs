248a3c95852ee808a3c2e30d53646d600db583ed
Vitaliy Orazov <viordash@mail.ru>
2022-04-17 14:44:29 +0300

    add DI

---
+++ Projects/TutoProxy/TutoProxy.Server/CommandLine/AppRootCommand.cs
@@ -0,0 +1,48 @@
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.CommandLine.Parsing;
+using System.Reflection;
+
+namespace TutoProxy.Server.CommandLine {
+    internal class AppRootCommand : RootCommand {
+        public AppRootCommand() : base("Прокси сервер TuTo") {
+            Add(new Argument<string>("host", "Local host address"));
+            Add(new Argument<string>("test"));
+            var tcpOption = PortsArgument.CreateOption("--tcp");
+            var udpOption = PortsArgument.CreateOption("--udp");
+            Add(tcpOption);
+            Add(udpOption);
+            Add(new Option<bool>("--verbose", "Show the verbose logs"));
+            AddValidator((result) => {
+                if(!result.Children.Any(x => x.GetValueForOption(tcpOption) != null || x.GetValueForOption(udpOption) != null)) {
+                    result.ErrorMessage = "tcp or udp options requried";
+                }
+            });
+        }
+
+        public new class Handler : ICommandHandler {
+            public string? Host { get; set; }
+            public string? Test { get; set; }
+            public PortsArgument? Udp { get; set; }
+            public PortsArgument? Tcp { get; set; }
+            public bool Verbose { get; set; }
+
+            public async Task<int> InvokeAsync(InvocationContext context) {
+                if(Host == null || (Tcp == null && Udp == null)) {
+                    return -1;
+                }
+
+                Console.WriteLine("{0} {1}", Assembly.GetExecutingAssembly().GetName().Name, Assembly.GetExecutingAssembly().GetName().Version);
+                Console.WriteLine("Прокси сервер TuTo, хост {0}, tcp-порты {1}, udp-порты {2}", Host, Tcp, Udp);
+
+                Console.WriteLine($"The value for host is: {Host}");
+                Console.WriteLine($"The value for test is: {Test}");
+                Console.WriteLine($"The value for tcpPorts is: {Tcp}");
+                Console.WriteLine($"The value for udpPorts is: {Udp}");
+                Console.WriteLine($"The value for verbose is: {Verbose}");
+
+                return 0;
+            }
+        }
+    }
+}
--- Projects/TutoProxy/TutoProxy.Server/Program.cs
+++ Projects/TutoProxy/TutoProxy.Server/Program.cs
@@ -1,41 +1,21 @@
 ﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
 using System.CommandLine.Parsing;
-using System.Reflection;
+using Microsoft.Extensions.Hosting;
 using TutoProxy.Server.CommandLine;
 
 class Program {
     public static async Task<int> Main(string[] args) {
-        var hostArg = new Argument<string>("host", "Local host address");
-        var testArg = new Argument<string>("test");
-        var udpPortsOptions = PortsArgument.CreateOption("--udp");
-        var tcpPortsOptions = PortsArgument.CreateOption("--tcp");
+        var runner = new CommandLineBuilder(new AppRootCommand())
+            .UseHost(_ => new HostBuilder(), (builder) => builder
+                .ConfigureServices((_, services) => {
 
-        var verboseOpt = new Option<bool>("--verbose", "Show the verbose logs");
-        var rootCommand = new RootCommand {
-            hostArg,
-            testArg,
-            tcpPortsOptions,
-            udpPortsOptions,
-            verboseOpt,
-        };
-
-        rootCommand.Description = "Прокси сервер TuTo";
-
-        rootCommand.SetHandler((string host, string test, PortsArgument tcpPorts, PortsArgument udpPorts, bool verbose) => {
-            Console.WriteLine($"The value for host is: {host}");
-            Console.WriteLine($"The value for test is: {test}");
-            Console.WriteLine($"The value for tcpPorts is: {tcpPorts}");
-            Console.WriteLine($"The value for udpPorts is: {udpPorts}");
-            Console.WriteLine($"The value for verbose is: {verbose}");
-
-            StartServer(host, tcpPorts, udpPorts, verbose);
-        }, hostArg, testArg, tcpPortsOptions, udpPortsOptions, verboseOpt);
-
-        return await rootCommand.InvokeAsync(args);
-    }
-
-    static void StartServer(string host, PortsArgument tcpPorts, PortsArgument udpPorts, bool verbose) {
-        Console.WriteLine("{0} {1}", Assembly.GetExecutingAssembly().GetName().Name, Assembly.GetExecutingAssembly().GetName().Version);
-        Console.WriteLine("Прокси сервер TuTo, хост {0}, tcp-порты {1}, udp-порты {2}", host, tcpPorts, udpPorts);
+                })
+                .UseCommandHandler<AppRootCommand, AppRootCommand.Handler>()
+             )
+            .UseDefaults()
+            .Build();
+        return await runner.InvokeAsync(args);
     }
 }
