73ace41e77158a79e4949f414b839c6309c6cc5a
Vitaliy Orazov <viordash@mail.ru>
2022-05-17 00:04:23 +0300

    refactoring DataTransferServiceTests

--- Projects/TutoProxy/TutoProxy.Server.Tests/Services/DataTransferServiceTests.cs
+++ Projects/TutoProxy/TutoProxy.Server.Tests/Services/DataTransferServiceTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
@@ -15,6 +12,8 @@ using TutoProxy.Server.Services;
 using TuToProxy.Core.Services;
 
 namespace TutoProxy.Server.Tests.Services {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
     public class DataTransferServiceTests {
         class TestableDataTransferService : DataTransferService {
             public TestableDataTransferService(ILogger logger, IIdService idService, IDateTimeService dateTimeService, IHubContext<DataTunnelHub> hubContext)
@@ -26,59 +25,54 @@ namespace TutoProxy.Server.Tests.Services {
             }
         }
 
-
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         TestableDataTransferService testable;
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
-        Mock<ILogger> loggerMock = new();
-        Mock<IIdService> idServiceMock = new();
-        Mock<IDateTimeService> dateTimeServiceMock = new();
-        Mock<IHubContext<DataTunnelHub>> hubContextMock = new();
-        Mock<IClientProxy> clientProxyMock = new();
+
+        Mock<IClientProxy> clientProxyMock;
 
         DateTime nowDateTime;
         string requestId = string.Empty;
+        TransferRequestModel? sendedTransferRequest;
 
         [SetUp]
         public void Setup() {
-            testable = new TestableDataTransferService(loggerMock.Object, idServiceMock.Object, dateTimeServiceMock.Object, hubContextMock.Object);
-
+            var loggerMock = new Mock<ILogger>();
+            var hubContextMock = new Mock<IHubContext<DataTunnelHub>>();
+            var idServiceMock = new Mock<IIdService>();
+            var dateTimeServiceMock = new Mock<IDateTimeService>();
             var hubClientsMock = new Mock<IHubClients>();
+            clientProxyMock = new();
+
             hubClientsMock
                 .SetupGet(x => x.All)
-                .Returns(() => {
-                    return clientProxyMock.Object;
-                });
+                .Returns(() => clientProxyMock.Object);
+
             hubContextMock
                 .SetupGet(x => x.Clients)
-                .Returns(() => {
-                    return hubClientsMock.Object;
-                });
+                .Returns(() => hubClientsMock.Object);
 
             requestId = "requestId";
             idServiceMock
                 .SetupGet(x => x.TransferRequest)
-                .Returns(() => {
-                    return requestId;
-                });
+                .Returns(() => requestId);
 
             nowDateTime = DateTime.Now;
             dateTimeServiceMock
                 .SetupGet(x => x.Now)
-                .Returns(() => {
-                    return nowDateTime;
-                });
-        }
+                .Returns(() => nowDateTime);
 
-        [Test]
-        public async Task RemoveExpiredRequests_Test() {
-            DataResponseModel? responseFromCallback = null;
-            TransferRequestModel? sendedTransferRequest = null;
+            sendedTransferRequest = null;
             clientProxyMock
                 .Setup(x => x.SendCoreAsync(It.IsAny<string>(), It.IsAny<object?[]>(), It.IsAny<CancellationToken>()))
                 .Callback<string, object?[], CancellationToken>((method, args, cancellationToken) => {
                     sendedTransferRequest = args[0] as TransferRequestModel;
                 });
+
+            testable = new TestableDataTransferService(loggerMock.Object, idServiceMock.Object, dateTimeServiceMock.Object, hubContextMock.Object);
+        }
+
+        [Test]
+        public async Task RemoveExpiredRequests_Test() {
+            DataResponseModel? responseFromCallback = null;
 
             var requestModel = new UdpDataRequestModel();
             requestId = Guid.NewGuid().ToString();
@@ -96,24 +90,20 @@ namespace TutoProxy.Server.Tests.Services {
             Assert.That(testable.PublicMorozovRequests.Keys, Has.Count.EqualTo(1));
 
             Assert.That(responseFromCallback, Is.Null);
+            Assert.That(sendedTransferRequest, Is.Not.Null);
 #pragma warning disable CS8604 // Possible null reference argument.
             testable.ReceiveResponse(new TransferResponseModel(sendedTransferRequest, new UdpDataResponseModel() { Data = new byte[] { 1, 2, 3, 4 } }));
 #pragma warning restore CS8604 // Possible null reference argument.
 
             Assert.That(responseFromCallback, Is.Not.Null);
             Assert.That(responseFromCallback?.Data, Is.EquivalentTo(new byte[] { 1, 2, 3, 4 }));
-        }
 
+            clientProxyMock.Verify(x => x.SendCoreAsync(It.Is<string>(m => m == "DataRequest"), It.IsAny<object?[]>(), It.IsAny<CancellationToken>()), Times.Exactly(3));
+        }
 
         [Test]
         public async Task ReceiveResponse_Is_Single_Action_Test() {
             DataResponseModel? responseFromCallback = null;
-            TransferRequestModel? sendedTransferRequest = null;
-            clientProxyMock
-                .Setup(x => x.SendCoreAsync(It.IsAny<string>(), It.IsAny<object?[]>(), It.IsAny<CancellationToken>()))
-                .Callback<string, object?[], CancellationToken>((method, args, cancellationToken) => {
-                    sendedTransferRequest = args[0] as TransferRequestModel;
-                });
 
             var requestModel = new UdpDataRequestModel();
             await testable.SendRequest(requestModel, (response) => { responseFromCallback = response; });
@@ -130,6 +120,7 @@ namespace TutoProxy.Server.Tests.Services {
             responseFromCallback = null;
             testable.ReceiveResponse(new TransferResponseModel(sendedTransferRequest, new UdpDataResponseModel() { Data = new byte[] { 1, 2, 3, 4 } }));
             Assert.That(responseFromCallback, Is.Null);
+            clientProxyMock.Verify(x => x.SendCoreAsync(It.Is<string>(m => m == "DataRequest"), It.IsAny<object?[]>(), It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
