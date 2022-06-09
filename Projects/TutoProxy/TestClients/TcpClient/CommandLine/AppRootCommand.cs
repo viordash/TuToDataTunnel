60efe4f5f8a0c1ccece4fad6ab5d2b7211e656e5
Vitaliy Orazov <viordash@mail.ru>
2022-06-10 01:56:04 +0300

    TcpClient. reconnecting after some consecutive errors

--- Projects/TutoProxy/TestClients/TcpClient/CommandLine/AppRootCommand.cs
+++ Projects/TutoProxy/TestClients/TcpClient/CommandLine/AppRootCommand.cs
@@ -54,55 +54,62 @@ namespace TutoProxy.Server.CommandLine {
                 var remoteEndPoint = new IPEndPoint(IPAddress.Parse(Ip), Port);
 
                 while(!applicationLifetime.ApplicationStopping.IsCancellationRequested) {
-                    using var tcpClient = new Socket(SocketType.Stream, ProtocolType.Tcp);
-                    try {
-                        await tcpClient.ConnectAsync(remoteEndPoint, applicationLifetime.ApplicationStopping);
-                    } catch(SocketException) {
-                        logger.Warning($"tcp({Port}) connect timeout");
-                        await Task.Delay(5_000, applicationLifetime.ApplicationStopping);
-                        continue;
-                    }
-                    var localPort = (tcpClient.LocalEndPoint as IPEndPoint)!.Port;
-
-                    var sRateStopWatch = new Stopwatch();
-                    var logTimer = DateTime.Now.AddSeconds(1);
-                    double sRate = 0;
-                    int packetsCount = 0;
-
-                    while(!applicationLifetime.ApplicationStopping.IsCancellationRequested) {
-                        var dataPacket = Enumerable.Repeat(Guid.NewGuid().ToByteArray(), (Packet / 16) + 1)
-                            .SelectMany(x => x)
-                            .Take(Packet).ToArray();
-                        sRateStopWatch.Restart();
-                        var txCount = await tcpClient.SendAsync(dataPacket, SocketFlags.None, applicationLifetime.ApplicationStopping);
-
+                    using(var tcpClient = new Socket(SocketType.Stream, ProtocolType.Tcp)) {
                         try {
-                            using var cts = CancellationTokenSource.CreateLinkedTokenSource(applicationLifetime.ApplicationStopping);
-                            cts.CancelAfter(TimeSpan.FromMilliseconds(5000));
-
-                            Memory<byte> receiveBuffer = new byte[Math.Max(16384, Packet)];
-                            var receivedBytes = await tcpClient.ReceiveAsync(receiveBuffer, SocketFlags.None, cts.Token);
-
-                            sRateStopWatch.Stop();
-                            if(dataPacket.SequenceEqual(receiveBuffer[..receivedBytes].ToArray())) {
-                                var ts = sRateStopWatch.Elapsed;
-                                sRate += receivedBytes / ts.TotalMilliseconds;
-                                packetsCount++;
-                                if(logTimer <= DateTime.Now) {
-                                    logTimer = DateTime.Now.AddSeconds(1);
-                                    logger.Information($"tcp({localPort}) response from {tcpClient.RemoteEndPoint}, bytes:{receivedBytes}, packets:{packetsCount}, srate:{(sRate / packetsCount):0} KB/s. Success");
-                                    sRate = 0;
-                                    packetsCount = 0;
-                                }
-                            } else {
-                                logger.Warning($"tcp({localPort}) response from {tcpClient.RemoteEndPoint}, bytes:{receivedBytes}. Wrong");
-                            }
-                        } catch(OperationCanceledException) {
-                            logger.Warning($"tcp({localPort}) response timeout");
+                            await tcpClient.ConnectAsync(remoteEndPoint, applicationLifetime.ApplicationStopping);
+                        } catch(SocketException) {
+                            logger.Warning($"tcp({Port}) connect timeout");
+                            await Task.Delay(5_000, applicationLifetime.ApplicationStopping);
+                            continue;
                         }
+                        var localPort = (tcpClient.LocalEndPoint as IPEndPoint)!.Port;
 
-                        if(Delay > 0) {
-                            await Task.Delay(Delay, applicationLifetime.ApplicationStopping);
+                        var sRateStopWatch = new Stopwatch();
+                        var logTimer = DateTime.Now.AddSeconds(1);
+                        double sRate = 0;
+                        int packetsCount = 0;
+                        int errors = 0;
+
+                        while(!applicationLifetime.ApplicationStopping.IsCancellationRequested) {
+                            var dataPacket = Enumerable.Repeat(Guid.NewGuid().ToByteArray(), (Packet / 16) + 1)
+                                .SelectMany(x => x)
+                                .Take(Packet).ToArray();
+                            sRateStopWatch.Restart();
+                            var txCount = await tcpClient.SendAsync(dataPacket, SocketFlags.None, applicationLifetime.ApplicationStopping);
+
+                            try {
+                                using var cts = CancellationTokenSource.CreateLinkedTokenSource(applicationLifetime.ApplicationStopping);
+                                cts.CancelAfter(TimeSpan.FromMilliseconds(5000));
+
+                                Memory<byte> receiveBuffer = new byte[Math.Max(16384, Packet)];
+                                var receivedBytes = await tcpClient.ReceiveAsync(receiveBuffer, SocketFlags.None, cts.Token);
+
+                                sRateStopWatch.Stop();
+                                if(dataPacket.SequenceEqual(receiveBuffer[..receivedBytes].ToArray())) {
+                                    var ts = sRateStopWatch.Elapsed;
+                                    sRate += receivedBytes / ts.TotalMilliseconds;
+                                    packetsCount++;
+                                    if(logTimer <= DateTime.Now) {
+                                        logTimer = DateTime.Now.AddSeconds(1);
+                                        logger.Information($"tcp({localPort}) response from {tcpClient.RemoteEndPoint}, bytes:{receivedBytes}, packets:{packetsCount}, srate:{(sRate / packetsCount):0} KB/s. Success");
+                                        sRate = 0;
+                                        packetsCount = 0;
+                                    }
+                                    errors = 0;
+                                } else {
+                                    logger.Warning($"tcp({localPort}) response from {tcpClient.RemoteEndPoint}, bytes:{receivedBytes}. Wrong");
+                                    await Task.Delay(TimeSpan.FromMilliseconds(2000), applicationLifetime.ApplicationStopping);
+                                    if(errors++ > 3) {
+                                        break;
+                                    }
+                                }
+                            } catch(OperationCanceledException) {
+                                logger.Warning($"tcp({localPort}) response timeout");
+                            }
+
+                            if(Delay > 0) {
+                                await Task.Delay(Delay, applicationLifetime.ApplicationStopping);
+                            }
                         }
                     }
                 }
