550d721130c58b50465b1dd2f8e821234128c9fc
Vitaliy Orazov <viordash@mail.ru>
2022-06-11 20:43:48 +0300

    clean code

--- Projects/TutoProxy/TutoProxy.Server/CommandLine/AppRootCommand.cs
+++ Projects/TutoProxy/TutoProxy.Server/CommandLine/AppRootCommand.cs
@@ -3,7 +3,6 @@ using System.CommandLine.Invocation;
 using System.Net;
 using System.Reflection;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TutoProxy.Core.CommandLine;
