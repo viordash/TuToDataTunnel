0e9e419fb1b48c7a5f5150ae537f66cafbe15dd1
Vitaliy Orazov <viordash@mail.ru>
2022-06-12 18:09:20 +0300

    fix naming

--- Projects/TutoProxy/TuToProxy.Core/Models/DataResponseModel.cs
+++ Projects/TutoProxy/TuToProxy.Core/Models/DataResponseModel.cs
@@ -2,7 +2,7 @@
 
 namespace TutoProxy.Core.Models {
     public class UdpDataResponseModel : DataBaseModel {
-        public UdpDataResponseModel(int port, int remotePort, byte[] data) : base(port, remotePort, data) {
+        public UdpDataResponseModel(int port, int originPort, byte[] data) : base(port, originPort, data) {
         }
 
         public override string ToString() {
@@ -11,7 +11,7 @@ namespace TutoProxy.Core.Models {
     }
 
     public class TcpDataResponseModel : DataBaseModel {
-        public TcpDataResponseModel(int port, int remotePort, byte[] data) : base(port, remotePort, data) {
+        public TcpDataResponseModel(int port, int originPort, byte[] data) : base(port, originPort, data) {
         }
         public override string ToString() {
             return $"tcp response: {base.ToString()}";
