cdbbf7a55474b3cb61be6824720a8b6489caf365
Vitaliy Orazov <viordash@mail.ru>
2022-06-13 23:25:20 +0300

    increase timeout periods

--- Projects/TutoProxy/TuToProxy.Core/UdpSocketParams.cs
+++ Projects/TutoProxy/TuToProxy.Core/UdpSocketParams.cs
@@ -1,6 +1,6 @@
 ﻿namespace TuToProxy.Core {
     public class UdpSocketParams {
-        public static TimeSpan ReceiveTimeout = TimeSpan.FromMilliseconds(5_000);
+        public static TimeSpan ReceiveTimeout = TimeSpan.FromMilliseconds(30_000);
         public static int LogUpdatePeriod = 1;
     }
 }
