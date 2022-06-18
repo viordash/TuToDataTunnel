61553b32c17c57705bafb42267550cb0410a65ec
Vitaliy Orazov <viordash@mail.ru>
2022-06-18 18:03:16 +0300

    beautify the bytes array in log message

--- Projects/TutoProxy/TuToProxy.Core/Extensions/ByteArrayExtensions.cs
+++ Projects/TutoProxy/TuToProxy.Core/Extensions/ByteArrayExtensions.cs
@@ -1,16 +1,17 @@
 ﻿namespace TuToProxy.Core.Extensions {
     public static class ByteArrayExtensions {
-        public static string ToShortDescriptions(this byte[] bytes) {
-            return bytes.Length switch {
-                1 => $"{bytes.Length} [{bytes[0]:X2}]",
-                2 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}]",
-                3 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}]",
-                4 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}]",
-                5 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}]",
-                6 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}{bytes[5]:X2}]",
-                7 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}{bytes[5]:X2}{bytes[6]:X2}]",
-                8 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}{bytes[5]:X2}{bytes[6]:X2}{bytes[6]:X2}]",
-                > 8 => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}...{bytes[^4]:X2}{bytes[^3]:X2}{bytes[^2]:X2}{bytes[^1]:X2}]",
+        public static string ToShortDescriptions(this byte[] bytes, bool showPayload = false) {
+
+            return (bytes.Length, showPayload) switch {
+                (1, true) => $"{bytes.Length} [{bytes[0]:X2}]",
+                (2, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}]",
+                (3, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}]",
+                (4, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}]",
+                (5, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}]",
+                (6, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}{bytes[5]:X2}]",
+                (7, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}{bytes[5]:X2}{bytes[6]:X2}]",
+                (8, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}{bytes[4]:X2}{bytes[5]:X2}{bytes[6]:X2}{bytes[6]:X2}]",
+                ( > 8, true) => $"{bytes.Length} [{bytes[0]:X2}{bytes[1]:X2}{bytes[2]:X2}{bytes[3]:X2}...{bytes[^4]:X2}{bytes[^3]:X2}{bytes[^2]:X2}{bytes[^1]:X2}]",
                 _ => $"{bytes.Length}"
             };
         }
