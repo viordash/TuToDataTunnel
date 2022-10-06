b72eab1a69579ed0885f0db4e1a4bca21da1d267
Vitaliy Orazov <viordash@mail.ru>
2022-10-07 00:31:07 +0300

    WIP. add HttpProxy test project

--- Projects/TutoProxy/TestClients/HttpProxy/CommandLine/AppRootCommand.cs
+++ Projects/TutoProxy/TestClients/HttpProxy/CommandLine/AppRootCommand.cs
@@ -1,13 +1,16 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using HttpMachine;
 using IHttpMachine.Model;
 using Microsoft.Extensions.Hosting;
+using static TutoProxy.Server.CommandLine.AppRootCommand;
 
 namespace TutoProxy.Server.CommandLine {
     internal class AppRootCommand : RootCommand {
@@ -74,9 +77,8 @@ namespace TutoProxy.Server.CommandLine {
             async Task HandleSocketAsync(Socket socket, CancellationToken cancellationToken) {
                 Memory<byte> receiveBuffer = new byte[65536];
 
-                var clientToProxyStream = new NetworkStream(socket);
+                var clientToProxyStream = new NetworkStream(socket, true);
 
-                // читаем HTTP запрос от клиента
                 int receivedBytes = await clientToProxyStream.ReadAsync(receiveBuffer, cancellationToken);
                 if(receivedBytes == 0) {
                     return;
@@ -85,7 +87,7 @@ namespace TutoProxy.Server.CommandLine {
                 var bytes = receiveBuffer[..receivedBytes].ToArray();
 
                 var request = Encoding.ASCII.GetString(bytes);
-                logger.Information($"request: {request}");
+                logger.Information($"request: {request.Length}");
 
                 using(var handler = new HttpParserDelegate())
                 using(var parser = new HttpCombinedParser(handler)) {
@@ -99,43 +101,131 @@ namespace TutoProxy.Server.CommandLine {
                             break;
 
                         default:
-                            await HttpProxyAsync(clientToProxyStream, handler.HttpRequestResponse, cancellationToken);
+                            await HttpProxyAsync(clientToProxyStream, handler.HttpRequestResponse, request, cancellationToken);
                             break;
                     }
                 }
                 logger.Information($"tcp disconnected {socket.RemoteEndPoint}");
+                socket.Dispose();
             }
 
+
+            static async Task<bool> WaitData(NetworkStream networkStream) {
+                var timer = DateTime.Now.AddMilliseconds(1000);
+                while(networkStream.CanRead && !networkStream.DataAvailable && timer > DateTime.Now) {
+                    await Task.Delay(1);
+                }
+                bool dataAvailable = networkStream.DataAvailable;
+                return dataAvailable;
+            }
 
             async Task HttpsProxyAsync(NetworkStream clientToProxyStream, HttpRequestResponse httpRequestResponse, CancellationToken cancellationToken) {
                 var uri = new Uri("http://" + httpRequestResponse.RequestUri);
 
-                var client = new TcpClient(uri.Host, uri.Port);
-                var proxyToStackoverflowStream = client.GetStream();
+                using var client = new TcpClient(uri.Host, uri.Port);
+                var proxyToClientStream = client.GetStream();
 
-                string response = $"HTTP/{httpRequestResponse.MajorVersion}.{httpRequestResponse.MinorVersion} 200 Connection established\r\n\r\n";
+                var response = $"HTTP/{httpRequestResponse.MajorVersion}.{httpRequestResponse.MinorVersion} 200 Connection established\r\n\r\n";
                 var bytes = Encoding.ASCII.GetBytes(response);
                 await clientToProxyStream.WriteAsync(bytes);
-                var task1 = clientToProxyStream.CopyToAsync(proxyToStackoverflowStream, cancellationToken);
-                var task2 = proxyToStackoverflowStream.CopyToAsync(clientToProxyStream, cancellationToken);
+
+                using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                cts.CancelAfter(TimeSpan.FromMilliseconds(3000));
+
+                var task1 = clientToProxyStream.CopyToAsync(proxyToClientStream, cts.Token);
+                var task2 = proxyToClientStream.CopyToAsync(clientToProxyStream, cts.Token);
                 await Task.WhenAll(task1, task2);
+
+                //int counter = 0;
+                //Memory<byte> receiveBuffer = new byte[65536];
+                //do {
+                //    int receivedBytes = await clientToProxyStream.ReadAsync(receiveBuffer, cancellationToken);
+                //    if(receivedBytes == 0) {
+                //        return;
+                //    }
+
+                //    logger.Information($"request: {Encoding.ASCII.GetString(receiveBuffer[..receivedBytes].ToArray()).Length}, cnt: {counter++}");
+
+                //    await proxyToClientStream.WriteAsync(receiveBuffer[..receivedBytes], cancellationToken);
+                //    //await proxyToClientStream.FlushAsync(cancellationToken);
+
+                //    do {
+                //        int clientReceivedBytes = await proxyToClientStream.ReadAsync(receiveBuffer, cancellationToken);
+                //        if(clientReceivedBytes == 0) {
+                //            return;
+                //        }
+                //        logger.Information($"response: {Encoding.ASCII.GetString(receiveBuffer[..clientReceivedBytes].ToArray()).Length}, cnt: {counter++}");
+
+                //        await clientToProxyStream.WriteAsync(receiveBuffer[..clientReceivedBytes]);
+                //        //await clientToProxyStream.FlushAsync(cancellationToken);
+
+                //    } while(await WaitData(proxyToClientStream));
+
+                //} while(await WaitData(clientToProxyStream));
+
+                await clientToProxyStream.Socket.DisconnectAsync(true, cancellationToken);
             }
 
-            async Task HttpProxyAsync(NetworkStream clientToProxyStream, HttpRequestResponse httpRequestResponse, CancellationToken cancellationToken) {
+            async Task HttpProxyAsync(NetworkStream clientToProxyStream, HttpRequestResponse httpRequestResponse, string request,
+                    CancellationToken cancellationToken) {
+
+                var uri = new Uri(httpRequestResponse.RequestUri);
+
+                using var client = new TcpClient(uri.Host, uri.Port);
+                //client.Connect(uri.Host, uri.Port);
+                //client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+                //client.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveTime, 2000);
+                //client.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveInterval, 2000);
+                //client.Client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.TcpKeepAliveRetryCount, 4);
+
+                //int size = Marshal.SizeOf((uint)0);
+                //byte[] keepAlive = new byte[size * 3];
+                //// Pack the byte array:
+                //// Turn keepalive on
+                //Buffer.BlockCopy(BitConverter.GetBytes((uint)1), 0, keepAlive, 0, size);
+                //// Set amount of time without activity before sending a keepalive to 5 seconds
+                //Buffer.BlockCopy(BitConverter.GetBytes((uint)5000), 0, keepAlive, size, size);
+                //// Set keepalive interval to 5 seconds
+                //Buffer.BlockCopy(BitConverter.GetBytes((uint)15000), 0, keepAlive, size * 2, size);
+
+                //// Set the keep-alive settings on the underlying Socket
+                //client.Client.IOControl(IOControlCode.KeepAliveValues, keepAlive, null);
 
 
+                var proxyToClientStream = client.GetStream();
 
-                //[01:48:54 I]request: POST http://tuto24.com/test HTTP/1.1
-                //User-Agent: PostmanRuntime/7.29.2
-                //Accept: */*
-                //Cache-Control: no-cache
-                //Postman-Token: d2fcbb01-e946-411b-9b55-7e932cc31469
-                //Host: tuto24.com
-                //Accept-Encoding: gzip, deflate, br
-                //Connection: keep-alive
-                //Cookie: PHPSESSID=10t68rqn5e0q04dje72oms8i30
-                //Content-Length: 0
+                var requestToClient = request.Replace(httpRequestResponse.RequestUri, uri.PathAndQuery);
+                //requestToClient = requestToClient.Replace("Connection: keep-alive", "Connection: close");
 
+                logger.Information($"requestToClient: {requestToClient.Length}");
+
+                using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+                cts.CancelAfter(TimeSpan.FromMilliseconds(1000));
+
+                var bytes = Encoding.ASCII.GetBytes(requestToClient);
+                try {
+                    await proxyToClientStream.WriteAsync(bytes, cts.Token);
+                    await proxyToClientStream.CopyToAsync(clientToProxyStream, cts.Token);
+
+                } catch(Exception e) {
+                    //    logger.Error(e.GetBaseException().ToString());
+                }
+
+                //int counter = 0;
+                //Memory<byte> receiveBuffer = new byte[65536];
+                //do {
+                //    int receivedBytes = await proxyToClientStream.ReadAsync(receiveBuffer, cancellationToken);
+                //    if(receivedBytes == 0) {
+                //        return;
+                //    }
+
+                //    logger.Information($"request: {Encoding.ASCII.GetString(receiveBuffer[..receivedBytes].ToArray()).Length}, cnt: {counter++}");
+
+                //    await clientToProxyStream.WriteAsync(receiveBuffer[..receivedBytes]);
+
+                //} while(await WaitData(proxyToClientStream));
+
+                await clientToProxyStream.Socket.DisconnectAsync(true, cancellationToken);
             }
 
         }
