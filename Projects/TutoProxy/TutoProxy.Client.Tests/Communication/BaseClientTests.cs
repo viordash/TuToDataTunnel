3d4645bd85682d12b4e7526ce987145ad917734a
Vitaliy Orazov <viordash@mail.ru>
2022-12-18 13:37:25 +0300

    add test for BaseClient

--- Projects/TutoProxy/TutoProxy.Client.Tests/Communication/BaseClientTests.cs
+++ Projects/TutoProxy/TutoProxy.Client.Tests/Communication/BaseClientTests.cs
@@ -7,29 +7,38 @@ using TutoProxy.Client.Services;
 namespace TutoProxy.Client.Tests.Communication {
     public class BaseClientTests {
 
-        public class TestableClient : BaseClient<Socket> {
-
-            public TestableClient(IPEndPoint serverEndPoint, int originPort, ILogger logger, IClientsService clientsService, ISignalRClient dataTunnelClient)
-                : base(serverEndPoint, originPort, logger, clientsService, dataTunnelClient) {
+        public class TestableClient : BaseClient {
+            public TestableClient(IPEndPoint serverEndPoint, int originPort, ILogger logger, IClientsService clientsService, ISignalRClient dataTunnelClient,
+                IProcessMonitor processMonitor)
+                : base(serverEndPoint, originPort, logger, clientsService, dataTunnelClient, processMonitor) {
             }
 
-            protected override Socket CreateSocket() {
-                return new Socket(serverEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-            }
-
+            public CancellationTokenSource PublicMorozov_CancellationTokenSource => cancellationTokenSource;
         }
-
 
         Mock<ILogger> loggerMock;
         Mock<IClientsService> clientsServiceMock;
         Mock<ISignalRClient> signalRClientMock;
+        Mock<IProcessMonitor> processMonitorMock;
 
         [SetUp]
         public void Setup() {
             loggerMock = new();
             clientsServiceMock = new();
             signalRClientMock = new();
+            processMonitorMock = new();
         }
 
+
+        [Test]
+        public async Task CancellationTokenSource_Canceled_On_Dispose_Test() {
+            var testable = new TestableClient(new IPEndPoint(IPAddress.Loopback, 80), 8001, loggerMock.Object, clientsServiceMock.Object, signalRClientMock.Object,
+                    processMonitorMock.Object);
+
+            Assert.That(testable.PublicMorozov_CancellationTokenSource.IsCancellationRequested, Is.False);
+
+            await testable.DisposeAsync();
+            Assert.That(testable.PublicMorozov_CancellationTokenSource.IsCancellationRequested, Is.True);
+        }
     }
 }
