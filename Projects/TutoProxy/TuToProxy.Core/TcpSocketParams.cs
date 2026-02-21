fceda67af697fa8432ceea22fc73f72f3debfc07
Vitalii Orazov <viordash@mail.ru>
2026-02-21 12:27:09 +0300

    feat: increase perf with TcpSocketParams

--- Projects/TutoProxy/TuToProxy.Core/TcpSocketParams.cs
+++ Projects/TutoProxy/TuToProxy.Core/TcpSocketParams.cs
@@ -1,13 +1,13 @@
 ﻿namespace TuToProxy.Core {
     public class TcpSocketParams {
-        public static int ReceiveBufferSize = 65536;
+        public static int ReceiveBufferSize = 65536 * 4;
         public static TimeSpan ReceiveTimeout = TimeSpan.FromMilliseconds(10_000);
         public static int LogUpdatePeriod = 1;
         public static bool TrafficMonitoring = true;
 
         // Pipeline parameters
-        public static int MaxInflightRequests = 4;
-        public static int ChannelCapacity = 16;
+        public static int MaxInflightRequests = 16;
+        public static int ChannelCapacity = 32;
         public static int PipePauseThreshold = 131072;   // 128KB
         public static int PipeResumeThreshold = 65536;   // 64KB
     }
